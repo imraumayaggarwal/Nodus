220660f92fd42b7df07a45e6c5268200f2137075
Raumay Aggarwal <aggarwalraumay@gmail.com>
2026-07-02 00:44:55 +0530

    feat: create main App component with Navbar and AddButton

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/src/components/Navbar/Navbar"
+import AddButton from "@/src/components/add-button/add-button";
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <AddButton />
+        </div>
+    );
+}
