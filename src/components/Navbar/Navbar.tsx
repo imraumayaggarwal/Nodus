1332777a3464b01c277c7c8ebd1f9179dc840ef5
Raumay Aggarwal <aggarwalraumay@gmail.com>
2026-07-03 03:04:49 +0530

    feat: enhance Navbar styling and update branding text

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,18 +7,20 @@ export default function Navbar() {
     const [darkMode, setDarkMode]= useState(false);
 
     return (
-        <nav>
+        <nav className="fixed top-0 left-0 right-0 z-50 flex justify-center pt-5 selection:bg-neutral-200">
             <div
                 onClick={() => setSelected(!selected)} 
-                className={`max-w-7xl flex border border-dotted hover:border-solid mt-5 mx-auto px-10 py-5 justify-between items-center shadow-black/10 transition-all duration-400 overflow-hidden hover:shadow-xl ${
+                className={`max-w-7xl flex border border-dotted backdrop-blur-md hover:border-blue-400  mx-auto px-10 py-5 justify-between items-center shadow-black/10 transition-all duration-500 overflow-hidden shadow-md ${
                     selected
-                        ? "w-44 px-2 py-5 shadow-md rounded-xl"
-                        : "w-[550px] px-10 py-5 justify-between shadow-md rounded-3xl"
+                        ? "w-44 px-2 py-5 rounded-xl"
+                        : "w-[850px] px-10 py-5 justify-between rounded-3xl"
                     }
                     
                     `}
             >
-                <div className='font-semibold select-none text-xl'>Listify</div>
+                <div className='font-semibold select-none text-xl'>
+                    Nodus
+                </div>
                 <div 
                     onClick={(e) => {
                         e.stopPropagation();
@@ -26,8 +28,8 @@ export default function Navbar() {
                     }}
                     className={`h-8 w-8 rounded-full transition-all duration-250 border shadow-md shadow-black/30 ${
                     darkMode
-                        ? "bg-gray-200 bg-[#F5F5DC]"
-                        : "bg-gray-800"  
+                        ? "bg-gray-200 hover:border-blue-500"
+                        : "bg-gray-900 hover:border-blue-500"  
                     } ${
                     selected 
                         ? "opacity-0 scale-0" 
